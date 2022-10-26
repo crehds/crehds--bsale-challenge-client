283935f960eec6d689234c003230e6c76b820730
Carlos Rodriguez <carlos15erh@gmail.com>
2022-10-25 22:20:43 -0500

    Add: spinner to better UX

---
+++ src/components/Spinner.js
@@ -0,0 +1,21 @@
+import Component from '../utils/Component.js';
+
+const render = () =>
+  `<div class="spinner-wrapper">
+            <div class="loading-wrapper">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>`;
+
+export default Component({
+  render,
+  listeners: [],
+  preloaders: []
+});
